d9cb9edaa05e28924ad048ea7db8e2c79a5adfa7
Alvaro Amoros <themaxter99@gmail.com>
2022-04-27 18:41:50 +0200

    update: Document-card, newpagecomponent DocumentModel

---
+++ backoffice-angular/src/app/shared/models/DocumentModel.ts
@@ -0,0 +1,11 @@
+export class Document {
+    public title!: string;
+    public description!: string;
+    public date!: Date;
+    public content!: string;
+    public author!: string;
+    public archiveDate?: Date ;
+
+    
+}
+
