43b8ba05506ebf943e7f58b083f88efa8c406784
Alvaro Amoros <themaxter99@gmail.com>
2022-04-28 16:55:19 +0200

    App Base V0.1

--- backoffice-angular/src/app/components/document-card/document-card.component.ts
+++ backoffice-angular/src/app/components/document-card/document-card.component.ts
@@ -13,34 +13,24 @@ export class DocumentCardComponent implements OnInit {
 
   constructor(private apiService: ApiServiceService) {}
 
-  ngOnInit(): void {
-    /*
-    this.documento = {
-      title: 'Titulo rexulon',
-      description: 'Lorem imsun asdasdasd',
-      date: new Date(),
-      content:
-        "ypeext of text of the printing and typeext of the printing and typeext of the printing and typeext of the printing and typeext of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has s",
-      author: 'El Maxter',
-    };
-    */
-  }
+  ngOnInit(): void {}
 
   archivarDocumento() {
     this.apiService.archivarDocument(this.documento._id!).subscribe(
       (data) => {
-        console.log(data)
+        console.log(data);
       },
       (err) => {
         console.log(err);
       }
     );
+    
   }
 
   eliminarDocumento() {
     this.apiService.deleteDocument(this.documento._id!).subscribe(
       (data) => {
-        console.log(data)
+        console.log(data);
       },
       (err) => {
         console.log(err);
